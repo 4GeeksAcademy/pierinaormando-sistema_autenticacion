0e212820fbe622de057a019b342f9c78eec4b7aa
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-12-23 00:01:27 +0000

    register, login and protected

--- src/api/models.py
+++ src/api/models.py
@@ -4,7 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    full_name = db.Column(db.String(60), unique=False, nullable=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -14,6 +15,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "full_name": self.full_name,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
